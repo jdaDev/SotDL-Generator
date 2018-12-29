66d12131b2fe7555597b3c70949fe671d16ba549
Jesse Alexander <jesse.d.alexander22@gmail.com>
2018-12-29 16:40:25 -0330

    Update javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var humanBackgrounds = [
+    'You died and returned to life. You start the game with 1d6 insanity.', // 1
+    'You were briefly possessed by a demon. You start the game with 1 corruption.', // 2
+    'You spent 1d6 years as a prisoner in a dungeon.',  // 3
+    'You murdered someone in cold blood. You start the game with 1 corruption.',    // 4
+    'You caught and recovered from a terrible disease.', // 5
+    'You belonged to a strange cult and saw many strange things. You start the game with 1 insanity.',  // 6
+    'The faerie held you prisoner for 1s20 years.', // 7
+    'You lost a loved one and their loss haunts you still.',    // 8
+    'You lost a finger, a few teeth, or an ear, or you gained a scar.',
+    'You earned a living working in your profession.',
+    'You fell in love and the relationship ended well or is ongoing.',
+    'You have a spouse and 1d6 - 2 children (minimum 0).',
+    'You traveled extensively. you speak one additional language.',
+    'You received an eductation. You know how to read the Common Tongue.',
+    'You saved your town from terrible monsters.',
+    'You foiled a plot to kill someone important or you brought a killer to justice.',
+    'You performed a great deed and are a hero to the people of your hometown.',
+    'You found an old treasure map.',
+    'Someone important and powerful owes you a favor.',
+    'You came into money and start tje game with 2d6 cp.'
+]

2e3924559556ad0e0ec4fc79e58917b76db722d6
Jesse Alexander <jesse.d.alexander22@gmail.com>
2019-01-24 13:30:00 -0330

    Adding Salamander ancestry tables

---
+++ JavaScript/salamanders.js
@@ -0,0 +1,53 @@
+/**
+ * Salamander Tables
+ */
+var salamanderAge =
+[
+    'You are a child, 8 years old or younger.',
+    'You are an adolescent, 9 to 15 years old.',
+    'You are an adolescent, 9 to 15 years old.',
+    'You are an adolescent, 9 to 15 years old.',
+    'You are an adolescent, 9 to 15 years old.',
+    'You are a young adult, 16 to 50 years old.',
+    'You are a young adult, 16 to 50 years old.',
+    'You are a young adult, 16 to 50 years old.',
+    'You are a young adult, 16 to 50 years old.',
+    'You are a young adult, 16 to 50 years old.',
+    'You are a middle-aged adult, 51 to 100 years old.',
+    'You are a middle-aged adult, 51 to 100 years old.',
+    'You are a middle-aged adult, 51 to 100 years old.',
+    'You are an older adult, 101 to 150 years old.',
+    'You are an older adult, 101 to 150 years old.',
+    'You are a venerable adult, 151 years or older.'
+]
+
+var salamanderPersonality = 
+[
+    'Hate consumes you. You enjoy hurting other people.',
+    'You have a nasty temper and resort to violence when angered.',
+    'You are a braggart and a bully.',
+    'You are a braggart and a bully.',
+    'You are rude to people you do not know and unfriendly to those you do know.',
+    'You are rude to people you do not know and unfriendly to those you do know.',
+    'You are bold and aggressive.',
+    'You are bold and aggressive.',
+    'You are bold and aggressive.',
+    'You are bold and aggressive.',
+    'You are friendly, chamring, and boisterous.',
+    'You are friendly, charming, and boisterous.',
+    'You try to do the right thing, but your passions sometimes get in the way.',
+    'You try to do the right thing, but your passions sometimes get in the way.',
+    'You have learned to control your emotions, so that you seem cold and reserved to others.',
+    'You have an infectious cheer. You put others at ease, and people want to be around you.'
+]
+
+var salamanderAppearance = 
+[
+    'You have a monstrous appearance that even your own people find unsettling. A profusion of horns and spurs ' +
+    'erupt from all over your body, and your skin is a sickly, blotchy color. Your visage is a snarling mass of bone ' +
+    'and flesh, eyes yellowed and dull.',
+    'You are a firetail, an offshoot of the salamander race born with a serpentine tail instead of legs. Aside from this mutation ' + 
+    ', your features are those of other salamanders. Your tail imposes a -2 penalty to your Speed but grants you 1 boon on attack rolls ' +
+    'made to grab.',
+    
+]

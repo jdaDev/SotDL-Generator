c9a620f64403ae59a48b6a985f1ec31c321d8290
Jesse Alexander <jesse.d.alexander22@gmail.com>
2019-01-24 13:48:48 -0330

    Update salamanders.js

--- JavaScript/salamanders.js
+++ JavaScript/salamanders.js
@@ -49,5 +49,70 @@ var salamanderAppearance =
     'You are a firetail, an offshoot of the salamander race born with a serpentine tail instead of legs. Aside from this mutation ' + 
     ', your features are those of other salamanders. Your tail imposes a -2 penalty to your Speed but grants you 1 boon on attack rolls ' +
     'made to grab.',
-    
+    'You have crude, brutish features with mottled orange-and-black skin. You are considered plain or even unattractive by your people.',
+    'You have crude, brutish features with mottled orange-and-black skin. You are considered plain or even unattractive by your people.',
+    'You have 1d6 + 3 toothlike horns growing from the top of your head. Your skin is rough and scaly, pierced by bizarre bone spurs all over your body.',
+    'You have 1d6 + 3 toothlike horns growing from the top of your head. Your skin is rough and scaly, pierced by bizarre bone spurs all over your body.',
+    'You have the appearance of a typical salamander.',
+    'You have the appearance of a typical salamander.',
+    'You have the appearance of a typical salamander.',
+    'You have the appearance of a typical salamander.',
+    'You are attractive for a salamander. Your horns are smooth and spurs small.',
+    'You are attractive for a salamander. Your horns are smooth and spurs small.',
+    'You are striking for a salamander. Your black horns are smooth and polished, your eyes blaze with an inner radiance, and your skin is warm and unbroken by spurs.',
+    'You are striking for a salamander. Your black horns are smooth and polished, your eyes blaze with an inner radiance, and your skin is warm and unbroken by spurs.',
+    'You are beautiful, even by the standards of other peoples. You have attractive features and a presence that grabs attention. You have no bone spurs, and only two small horns sprout from your forehead.',
+    'Your appearance sets you apart from nearly all of your kind. You have a shapely or muscular form with striking features. The horns and spurs ' +
+    'common to your race are arranged in such a way that almost everyone finds you appealing and perhaps a little exotic.'
 ]
+
+var salamanderBackground =
+[
+    'You nearly died, and your body has suffered as a result. You partly turned to flame and now appear scorched and are missing a few of your extremities.',
+    'You discovered the gates of Hell and passed through them to live among the devils for 1d3 years. You start the game with 1d3 Insanity and 1 Corruption.',
+    'You were kidnapped and enslaved by a wicked magician. After 1d6 years of abuse and exploitation, you escaped.',
+    'You committed a crime against your people and ﬂ ed from your homeland to escape the repercussions.',
+    'Undead besieged and destroyed your home. You are the only survivor.',
+    'You brieﬂy fell in with a cult dedicated to a monstrous being known as the Inferno. You broke with the cultists once they started summoning demons. ' +
+    'You start the game with 1 Corruption.',
+    'You ventured into the Desolation and survived to tell of it.',
+    'You lost someone close to you.',
+    'You were captured by travelers and sold as a slave in the Nine Cities. You only recently won your freedom.',
+    'You earned a living working in your profession.',
+    'You fell in love with someone of a different ancestry. You could have had a relationship with another character, if you both agree.',
+    'You have a spouse and 1d6 - 2 children (minimum 0).',
+    'You left your home long ago to see the world. You speak one additional language.',
+    'Your parents were leaders in your community, and you have lived in their shadow your whole life.',
+    'You saved the life of someone important, and that person now owes you a favor.',
+    'You were whisked away from your homeland by a magician who summoned you to serve him or her for a time.',
+    'Orphaned, you were found by a druid who raised you in the ways of the Old Faith.',
+    'You found something unusual in the Desolation. Start the game with one item from the Interesting Things of the Desolation table (this is in addition ' +
+    'to the interesting thing that all characters start with).',
+    'You helped defend your community. You start the game with a spear.',
+    'You came into money and start the game with 2d6 cp.'
+]
+
+var salamanderBuild = 
+[
+    'You are short and slender.',
+    'You are short and muscular.',
+    'You are short for your people.',
+    'You are short for your people.',
+    'You are thin, willowy, or gaunt.',
+    'You are thin, willowy, or gaunt.',
+    'You have typical height and weight for your people.',
+    'You have typical height and weight for your people.',
+    'You have typical height and weight for your people.',
+    'You have typical height and weight for your people.',
+    'You are somewhat overweight.',
+    'You are somewhat overweight.',
+    'You are tall.',
+    'You are tall.',
+    'You are tall and thin.',
+    'You tower over other salamanders, your body hulking and powerful.'
+]
+
+
+/**
+ * Salamander Functions
+ */

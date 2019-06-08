1455e336ea4da9a5d5bfad857efee38b66cb10a8
Jesse Alexander <jesse.d.alexander22@gmail.com>
2019-06-07 20:12:39 -0400

    Centered text + rollCharacter() function
    - Making all text appear in center of the page.
    
    - Added rollCharacter() function to roll appropriate ancestry depending on what is selected.

--- JavaScript/generalFunctions.js
+++ JavaScript/generalFunctions.js
@@ -2,3 +2,15 @@ function resetPage()
 {
     location.reload();
 }
+
+function rollCharacter(){
+    var e = document.getElementById("ancestry");
+    var selectedAncestry = e.value;
+
+    if(selectedAncestry == "changling"){
+        createCompleteChanglingCharacter();
+    }
+    else if(selectedAncestry == "human"){
+        createCompleteHumanCharacter();
+    }
+}

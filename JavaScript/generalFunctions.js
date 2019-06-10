c5305999e8809b5cd8a4ccf5805b23126919acfb
Jesse Alexander <jesse.d.alexander22@gmail.com>
2019-06-09 23:00:13 -0400

    Fixes

--- JavaScript/generalFunctions.js
+++ JavaScript/generalFunctions.js
@@ -43,44 +43,42 @@ function rollCharacter(){
     clearBox("clockworkPersonalityDisplay");
 
     // dwarf
-    clearBox("changlingBackgroundDisplay");
-    clearBox("changlingPersonalityDisplay");
-    clearBox("changlingTrueAgeDisplay");
-    clearBox("changlingApparentAncestryDisplay");
-    clearBox("changlingApparentGenderDisplay");
-    clearBox("changlingQuirkDisplay");
+    clearBox("dwarfAgeDisplay");
+    clearBox("dwarfBuildDisplay");
+    clearBox("dwarfAppearanceDisplay");
+    clearBox("dwarfHatredDisplay");
+    clearBox("dwarfBackgroundDisplay");
+    clearBox("dwarfPersonalityDisplay");
 
     // goblin
-    clearBox("changlingBackgroundDisplay");
-    clearBox("changlingPersonalityDisplay");
-    clearBox("changlingTrueAgeDisplay");
-    clearBox("changlingApparentAncestryDisplay");
-    clearBox("changlingApparentGenderDisplay");
-    clearBox("changlingQuirkDisplay");
+    clearBox("goblinBackgroundDisplay");
+    clearBox("goblinPersonalityDisplay");
+    clearBox("goblinOddHabitDisplay");
+    clearBox("goblinAgeDisplay");
+    clearBox("goblinBuildDisplay");
+    clearBox("goblinDistinctiveAppearanceDisplay");
 
     // orc
-    clearBox("changlingBackgroundDisplay");
-    clearBox("changlingPersonalityDisplay");
-    clearBox("changlingTrueAgeDisplay");
-    clearBox("changlingApparentAncestryDisplay");
-    clearBox("changlingApparentGenderDisplay");
-    clearBox("changlingQuirkDisplay");
+    clearBox("orcAgeDisplay");
+    clearBox("orcBuildDisplay");
+    clearBox("orcAppearanceDisplay");
+    clearBox("orcBackgroundDisplay");
+    clearBox("orcPersonalityDisplay");
 
     // faun
-    clearBox("changlingBackgroundDisplay");
-    clearBox("changlingPersonalityDisplay");
-    clearBox("changlingTrueAgeDisplay");
-    clearBox("changlingApparentAncestryDisplay");
-    clearBox("changlingApparentGenderDisplay");
-    clearBox("changlingQuirkDisplay");
+    clearBox("faunAgeDisplay");
+    clearBox("faunBuildDisplay");
+    clearBox("faunAppearanceDisplay");
+    clearBox("faunBackgroundDisplay");
+    clearBox("faunPersonalityDisplay");
 
     // halfling
-    clearBox("changlingBackgroundDisplay");
-    clearBox("changlingPersonalityDisplay");
-    clearBox("changlingTrueAgeDisplay");
-    clearBox("changlingApparentAncestryDisplay");
-    clearBox("changlingApparentGenderDisplay");
-    clearBox("changlingQuirkDisplay");
+    clearBox("halflingAgeDisplay");
+    clearBox("halflingBuildDisplay");
+    clearBox("halflingAppearanceDisplay");
+    clearBox("halflingBackgroundDisplay");
+    clearBox("halflingReligionDisplay");
+    clearBox("halflingPersonalityDisplay");
 
     // if changling is selected, clear text and create character.
     if(selectedAncestry == "changling"){
@@ -107,85 +105,4 @@ function rollCharacter(){
     else if(selectedAncestry == "halfling"){
         createCompleteHalflingCharacter();
     }
-}
- /*FUNCTIONS FOR CLEARING CHARACTER TEXT TO MAKE WAY FOR NEW CHARACTER*/
-
-// Function to clear human character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completePersonalityDisplay").innerHTML = "";
-    document.getElementById("completeReligionDisplay").innerHTML = "";
-    document.getElementById("completeAgeDisplay").innerHTML = "";
-    document.getElementById("completeBuildDisplay").innerHTML = "";
-    document.getElementById("completeAppearanceDisplay").innerHTML = "";
-}
-
-// Function to clear changling character
-function clearChangling(){
-    document.getElementById("changlingBackgroundDisplay").innerHTML = "";
-    document.getElementById("changlingPersonalityDisplay").innerHTML = "";
-    document.getElementById("changlingTrueAgeDisplay").innerHTML = "";
-    document.getElementById("changlingApparentAncestryDisplay").innerHTML = "";
-    document.getElementById("changlingApparentGenderDisplay").innerHTML = "";
-    document.getElementById("changlingQuirkDisplay").innerHTML = "";
-}
-
-// Function to clear clockwork character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-}
-
-// Function to clear dwarf character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-}
-
-// Function to clear goblin character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-}
-
-// Function to clear orc character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-}
-
-// Function to clear faun character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-}
-
-// Function to clear halfling character
-function clearHuman(){
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
-    document.getElementById("completeBackgroundDisplay").innerHTML = "";
 }

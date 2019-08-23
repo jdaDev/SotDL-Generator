2ba8c554a39f91eb65f2912dbff7a25642e4b143
Jesse Alexander <jesse.d.alexander22@gmail.com>
2019-08-23 12:36:00 -0400

    Changed some coloring in the flex boxes
    - Temporary for the sake of easy viewing of boxes when they resize.

---
+++ JavaScript/hamadryads.js
@@ -0,0 +1,43 @@
+/**
+ * Hamadryad Tables
+ */
+
+ var hamadryadAge =
+ [
+     'You are young, less than 50 years old.',
+     'You are an adult and have stopped physically aging. You are'
+     + '1d20 x 20 years old. For each century you have lived, add either one wilderness '
+     + 'profession to your list of professions or one language to your list of known languages.',
+     'You are an adult and have stopped physically aging. You are'
+     + '1d20 x 20 years old. For each century you have lived, add either one wilderness '
+     + 'profession to your list of professions or one language to your list of known languages.'
+ ]
+
+ var hamadryadTrueForm = 
+ [
+     'Walnut',
+     'Oak',
+     'Dogwood',
+     'Mulberry',
+     'Black poplar',
+     'Elm',
+     'Fig',
+     'Pine',
+     'Maple',
+     'Cedar',
+     'Willow',
+     'Chestnut',
+     'Hawthorn',
+     'Ash',
+     'Alder',
+     'Beech',
+     'Birch',
+     'Elder',
+     'Rowan',
+     'Holly'
+ ]
+
+ var hamadryadBackground =
+ [
+     
+ ]
